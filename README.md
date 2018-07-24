a5447badd06be96c17b2953b3c99ff30f6c691ab
忘篱 <chengli.ycl@alibaba-inc.com>
2018-07-24 16:57:07 +0800

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # rtc-app-csharp
+
 C# AppServer for RTC.
+
+For all languages:
+
+* [Python](https://github.com/winlinvip/rtc-app-python).
+* [Java](https://github.com/winlinvip/rtc-app-java).
+* [Golang](https://github.com/winlinvip/rtc-app-golang).
+* [PHP](https://github.com/winlinvip/rtc-app-php).
+* [C#/.Net](https://github.com/winlinvip/rtc-app-csharp).
+
+## Usage
+
+1. Generate AK from [here](https://usercenter.console.aliyun.com/#/manage/ak):
+
+```
+AccessKeyID: OGAEkdiL62AkwSgs
+AccessKeySecret: 4JaIs4SG4dLwPsQSwGAHzeOQKxO6iw
+```
+
+2. Create APP from [here](https://rtc.console.aliyun.com/#/manage):
+
+```
+AppID: iwo5l81k
+```
---
+++ rtc-app-csharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+
+namespace rtc_app_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appid = "", listen = "", accessKeyId = "", accessKeySecret = "", gslb = "";
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                string key = args[i];
+                if (key == "--appid")
+                {
+                    appid = args[++i];
+                }
+                else if (key == "--listen")
+                {
+                    listen = args[++i];
+                }
+                else if (key == "--access-key-id")
+                {
+                    accessKeyId = args[++i];
+                }
+                else if (key == "--access-key-secret")
+                {
+                    accessKeySecret = args[++i];
+                }
+                else if (key == "--gslb")
+                {
+                    gslb = args[++i];
+                }
+            }
+            if (appid == "" || listen == "" || accessKeyId == "" || accessKeySecret == "" || gslb == "")
+            {
+                System.Console.WriteLine(String.Format("Usage: <options> %s", args[0]));
+                Environment.Exit(-1);
+            }
+            System.Console.WriteLine(String.Format(
+                "Server listen=%s, appid=%s, akID=%s, akSecret=%s, gslb=%s",
+                listen, appid, accessKeyId, accessKeySecret, gslb));
+
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Start();
+            }
+        }
+    }
+}
